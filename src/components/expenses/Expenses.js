4d6f631bea3e80944a103451f1f2f242f53b9efc
Edgars Rumkovskis <edgars.rumkovskis@gmail.com>
2021-10-16 00:02:40 +0300

    - add year selection for filtering  - implement year filter state in Expenses.js  - implement expense filtering by year

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,20 +1,30 @@
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../ui/Card';
 
 import './Expenses.css';
-
+import {useState} from 'react';
 
 function Expenses(props) {
+
+  const [year, setYear] = useState('');
+
+  const filterByYearHandler = (year) => setYear(year);
+
+  const filterByYear = (expense) => year === '' || expense.date.getFullYear().toString() === year
+
   return <Card className="expenses">
+    <ExpensesFilter onFilterByYear={filterByYearHandler}/>
     {
-      props.expenses.map((expense =>
+      props.expenses
+        .filter(filterByYear)
+        .map(expense =>
           <ExpenseItem
             key={expense.id}
             date={expense.date}
             title={expense.title}
             amount={expense.amount}
-          />
-      ))
+          />)
     }
   </Card>
 }
